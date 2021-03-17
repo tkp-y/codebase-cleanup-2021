2f8dfd6f1427c5beb6c0fb4bdd55f1811f407734
tkp-y <tfk15@georgetown.edu>
2021-03-17 18:04:45 -0400

    WIP - started to reduce complexity

--- app/game.py
+++ app/game.py
@@ -43,3 +43,24 @@ elif u == "scissors" and c == "paper":
     print("The user wins")
 elif u == "scissors" and c == "scissors":
     print("It's a tie!")
+
+
+if u == "rock":
+    if c == "rock":
+        print("It's a tie!")
+    elif c == "paper":
+        print("The computer wins")
+    elif c == "scissors":
+        print("The user wins")
+elif u == "paper":
+    if c == "rock":
+        
+
+elif u == "scissors":
+
+
+#use a single dictionary
+
+
+
+
