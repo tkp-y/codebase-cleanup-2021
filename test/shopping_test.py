991d33f807f11537b31cc33f97d39bc7e7202a8c
tkp-y <tfk15@georgetown.edu>
2021-03-22 18:15:58 -0400

    Test price formatting logic

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,4 +1,11 @@
 
-# TODO: import some code
+#import the code we want to test
+from app.shopping import format_usd
+
+def test_format_usd():
+    assert format_usd(9.5) == "$9.50"
+
 
 # TODO: test the code
+
+
