060800bcc880fe4f6f64afb2265d0b460669a44e
tkp-y <tfk15@georgetown.edu>
2021-03-28 16:26:30 -0400

    Finish testing for shopping

--- app/shopping.py
+++ app/shopping.py
@@ -13,6 +13,19 @@ def format_usd(my_price):
     """
     return f"${my_price:,.2f}"
 
+def find_product(product_id, all_products):
+    """
+    Params :
+        product_id (str) like "8"
+        all_products (list of dict) each dict should have "id", "name", "department", "aisle", and "price" attributes
+    """
+    matching_products = [p for p in all_products if str(p["id"]) == str(product_id)]
+    if any(matching_products):
+        return matching_products[0]
+    else:
+        return None
+
+
 if __name__ == "__main__":
 
     # READ INVENTORY OF PRODUCTS
@@ -29,9 +42,9 @@ if __name__ == "__main__":
         if selected_id.upper() == "DONE":
             break
         else:
-            matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-            if any(matching_products):
-                selected_products.append(matching_products[0])
+            matching_product = find_product(selected_id, products)
+            if matching_product:
+                selected_products.append(matching_product)
             else:
                 print("OOPS, Couldn't find that product. Please try again.")
 
--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,11 +1,34 @@
 
 #import the code we want to test
-from app.shopping import format_usd
+import os
+import pytest
+from pandas import read_csv
+
+from app.shopping import format_usd, find_product
 
 def test_format_usd():
     assert format_usd(9.5) == "$9.50"
 
 
-# TODO: test the code
+@pytest.fixture(scope="module")
+def mock_products():
+    mock_products_filepath = os.path.join(os.path.dirname(__file__), "mock_data", "mock_products.csv")
+    mock_products_df = read_csv(mock_products_filepath)
+    return mock_products_df.to_dict("records")
+
+
+def test_find_product(mock_products):
+    # with valid product id, returns the product info:
+    valid_result = find_product("8", mock_products)
+    assert valid_result == {
+        'aisle': 'Aisle C',
+        'department': 'snacks',
+        'id': 8,
+        'name': 'Product 8',
+        'price': 10.0
+    }
+    # with invalid product id, returns None:
+    invalid_result = find_product("88888888", mock_products)
+    assert invalid_result == None
 
 
